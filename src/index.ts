cf4a8ac53e73bd6bfeee3a7e526de450f75493ec
Jakub Brambor <kuba.brambor@gmail.com>
2023-10-20 23:46:26 +0200

    ch10 / add objects to test types

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,7 @@
-function calculateTax(amount: number): number {
-  return amount * 1.2;
-}
+let hat = { name: 'czapka', price: 100 };
+let gloves = { name: 'rękawiczki', price: 75 };
+let jacket = { name: 'kurtka' };
 
-function writePrice(product: string, price: number): void {
-  console.log(`Cena produktu ${product}: ${price.toFixed(2)} zł`);
-}
+let products: { name: string, price: number }[] = [hat, gloves, jacket];
 
-let hat: [string, number] = ["czapka", 100];
-let gloves: [string, number]= ["rękawiczki", 75];
-let products: ([string, number] | boolean)[] = [true, false, hat]
-
-hat.forEach((el: string | number) => {
-  if (typeof el === "string") {
-    console.log(`Ten element to string ${el}`);
-  } else {
-    console.log(`Ten element to number ${el.toFixed(2)}`)
-  }
-})
-
-// writePrice(hat[0], hat[1]);
-// writePrice(gloves[0], gloves[1]);
-
-products.forEach((prod: [string, number] | boolean) => {
-  if(prod instanceof Array) {
-    writePrice(prod[0], prod[1]);
-  } else {
-    console.log('Its boolean.')
-  }
-})
+products.forEach(prod => console.log(`${prod.name} kosztuje ${prod.price}`));
