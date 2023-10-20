f70b8a1b4dea3d2fafef6bbf7c42cf725132228c
Jakub Brambor <kuba.brambor@gmail.com>
2023-10-21 00:09:17 +0200

    ch10 / type for method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+enum Feature { Waterproof, Insulated }
 let hat = { name: 'czapka', price: 100 };
 let gloves = { name: 'rękawiczki', price: 75 };
-let jacket = { name: 'kurtka' };
+let jacket = { name: 'kurtka', hasFeature: (feature) => feature === Feature.Waterproof };
 
-let products: { name: string, price: number }[] = [hat, gloves, jacket];
+let products: { name: string, price?: number, hasFeature?(Feature): boolean }[] = [hat, gloves, jacket];
 
-products.forEach(prod => console.log(`${prod.name} kosztuje ${prod.price}`));
+products.forEach(prod => console.log(`${prod.name} kosztuje ${prod.price}. Przemakalność ${prod.hasFeature(Feature.Waterproof)}`));
