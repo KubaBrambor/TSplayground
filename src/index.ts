77c7c2187859a60bec214111858d6f62dec840ae
Jakub Brambor <kuba.brambor@gmail.com>
2023-10-23 21:34:24 +0200

    ch10 / prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
-enum Feature { Waterproof, Insulated }
-let hat = { name: 'czapka', price: 100 };
-let gloves = { name: 'rękawiczki', price: 75 };
-let jacket = { name: 'kurtka', hasFeature: (feature) => feature === Feature.Waterproof };
+enum Feature {
+  Waterproof,
+  Insulated,
+}
+let hat = { name: "czapka", price: 100 };
+let gloves = { name: "rękawiczki", price: 75 };
+let jacket = {
+  name: "kurtka",
+  hasFeature: (feature) => feature === Feature.Waterproof,
+};
 
-let products: { name: string, price?: number, hasFeature?(Feature): boolean }[] = [hat, gloves, jacket];
+let products: {
+  name: string;
+  price?: number;
+  hasFeature?(Feature): boolean;
+}[] = [hat, gloves, jacket];
 
-products.forEach(prod => console.log(`${prod.name} kosztuje ${prod.price}. Przemakalność ${prod.hasFeature(Feature.Waterproof)}`));
+products.forEach((prod) =>
+  console.log(
+    `${prod.name} kosztuje ${prod.price}. Przemakalność ${
+      prod.hasFeature ? prod.hasFeature(Feature.Waterproof) : "false"
+    }`
+  )
+);
